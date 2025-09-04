03d63291bffabf6ae10f99794277a7d9e443c7d2
HanSide <ihanaulia@gmail.com>
2025-09-04 14:01:23 +0700

    Login Page/UI/MRA

--- lib/widgets/custom_card.dart
+++ lib/widgets/custom_card.dart
@@ -1,34 +1,59 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:kerjain/colors/app_colors.dart';
 
-class CustomCard extends StatelessWidget{
-
+class CustomCard extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry padding;
   final Color color;
-  final double borderRadius;
+  final bool hasShadow;
+  final double elevation;
+  final List<BoxShadow>? customShadow;
+  final double topRadius;
+  final double? height;
+  final double width;  
 
   const CustomCard({
     super.key,
     required this.child,
-    this.padding = const EdgeInsets.all(16.0),
-    this.color = ColorPalette.backgroundColor,
-    this.borderRadius = 8.0,
+    this.padding = const EdgeInsets.all(24.0),
+    this.color = Colors.white,
+    this.hasShadow = true,
+    this.elevation = 8.0,
+    this.customShadow,
+    this.topRadius = 20.0,
+    this.width = double.infinity,
+    this.height,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: padding,
+      width: width,
+      height: height,
       decoration: BoxDecoration(
         color: color,
-        borderRadius: BorderRadius.circular(borderRadius),
-        boxShadow: [
-          BoxShadow(
-            color: ColorPalette.shadowColor,
-            offset: const Offset(0, 2),
-          ),
-        ],
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(topRadius),
+          topRight: Radius.circular(topRadius),
+        ),
+        boxShadow: hasShadow
+            ? customShadow ??
+                [
+                  BoxShadow(
+                    color: ColorPalette.shadowColor,
+                    offset: const Offset(0, 4),
+                    blurRadius: 1,
+                    spreadRadius: 0,
+                  ),
+                  BoxShadow(
+                    color: ColorPalette.shadowColor,
+                    offset: const Offset(0, 2),
+                    blurRadius: 1,
+                    spreadRadius: 0,
+                  ),
+                ]
+            : null,
       ),
       child: child,
     );
--- lib/widgets/custom_text.dart
+++ lib/widgets/custom_text.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:kerjain/colors/app_colors.dart';
 
-class CustomText extends StatelessWidget{
-
+class CustomText extends StatelessWidget {
   final String text;
-  final TextStyle style;
+  final TextStyle? style;
+  final String myFont;
 
   const CustomText({
     super.key,
     required this.text,
-    this.style = const TextStyle(fontSize: 16.0, color: ColorPalette.shadowColor),
+    this.style,
+    this.myFont = 'Poppins',
   });
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: style,
+      style: (style ?? const TextStyle(fontSize: 16.0, color: ColorPalette.shadowColor)).copyWith(
+        fontFamily: myFont,
+      ),
     );
   }
 }
